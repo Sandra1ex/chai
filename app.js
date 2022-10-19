ff265c490ce15581c8025bf999fdf24fad04231a
Alexandra <lubnina.alexandra@gmail.com>
2022-10-19 15:35:13 +0300

    added jsx and js for main page

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ require('@babel/register');
 const express = require('express');
 const configApp = require('./config/serverConfig');
 
-const registrationRouter = require('./router/render/registrationRouter');
+const indexRouter = require('./routes/indexRouter');
+const teaRouter = require('./routes/teaRouter');
+// const registrationRouter = require('./router/render/registrationRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 configApp(app);
 
-app.use('/registration', registrationRouter);
+app.use('/', indexRouter);
+app.use('/tea', teaRouter);
+// app.use('/registration', registrationRouter);
 
 app.listen(PORT, () => console.log(`Сервер запущен, порт: ${PORT}`));
--- view/Main.jsx
+++ view/Main.jsx
@@ -1,11 +1,19 @@
 const React = require('react');
 const Layout = require('./Layout');
 const Header = require('./Header');
+const TeaCard = require('./TeaCard');
 
-function Main({ title, user }) {
+function Main({ title, user, teas }) {
   return (
     <Layout title={title}>
       <Header user={user} />
+      <div className="container js-tea-container">
+        {
+          teas.map((tea) => (
+            <TeaCard key={tea.id} tea={tea} />
+          ))
+        }
+      </div>
     </Layout>
   );
 }
