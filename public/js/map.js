3be518aff8cf4b3666682b6b886612724555a729
Alexandra <lubnina.alexandra@gmail.com>
2022-10-20 15:02:30 +0300

    feat: placemark of map

--- public/js/map.js
+++ public/js/map.js
@@ -11,5 +11,25 @@ function init() {
     zoom: 2,
   });
 
+  const placemark = new ymaps.Placemark([39.901698, 116.391433], {
+    balloonContentHeader: 'Хедер балуна',
+    balloonContentBody: 'Боди балуна',
+    balloonContentFooter: 'Подвал',
+  }, {
+    iconLayout: 'default#image',
+    iconImageHref: '/img/tea.png',
+    iconImageSize: [20, 20],
+    iconImageOffset: [0, 0],
+  });
+
   map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  map.geoObjects.add(placemark);
 }
